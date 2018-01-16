05a18a1ea63cdcce0d3bf095851be179e67c36ef
Saleh Osman <saleh1.a.osman@gmail.com>
2018-01-16 13:25:39 +0100

    Add project files.

---
+++ CelToFerTmp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using CelToFerTmp.Models;//when we use Class as input to ActionResult we have to add This Using Models
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CelToFerTmp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+           // CheckTemp.CalcTemp();
+            return View();
+        }
+     
+        [HttpPost]
+        public ActionResult Index(string name,string hypo, string scale, int temp=0)
+        {
+            if (name!="")
+            {
+                CheckTemp.Name = name;
+                CheckTemp.Temp =temp;
+
+                  if (hypo == "hypothermia" && CheckTemp.Temp<32 )
+                  {
+                      ViewBag.hypo = "There is hypothermia";
+                  }
+                  else if (hypo == "hypothermia" && CheckTemp.Temp >= 32)
+                  {
+                      ViewBag.hypo = "There is no hypothermia";
+                  }
+                  else
+                  {
+                     ViewBag.hypo = "";
+                  }
+
+                  if (scale == "celsius")
+                  {
+                    CheckTemp.CalcTemp();
+                    ViewBag.Fah = "";
+                  }
+                  else 
+                  {
+                      CheckTemp.CalcTemp();
+                      ViewBag.Fah = CheckTemp.Temp * (9 / 5) + 32;
+                  }
+            }
+            else
+            {
+                ViewBag.err="error,fill the name of patient";
+            }
+            return View();
+        }
+    }
+}
---
+++ CelToFerTmp/Models/CheckTemp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CelToFerTmp.Models
+{
+    public class CheckTemp
+    {
+        //we need data "using System.ComponentModel.DataAnnotations;" to use this []
+        //[Required(ErrorMessage ="input your name")]
+        //[Display(Name="Your name")]
+        //[Required]
+        static public string Name { get; set; }
+
+        //[Required(ErrorMessage ="input temprature")]
+        //[Display(Name ="Your temperature")]
+        //[Required]
+        static public int Temp { get; set; }
+
+        static public string DescriptionFever { get; set; }
+        static public string DescriptionHypo { get; set; }
+
+        static public void CalcTemp()
+        {
+            if (Temp<=38)
+            {
+                DescriptionFever = "There is no fever";
+            }
+            else
+            {
+                DescriptionFever = "There is fever";
+            }
+
+        }
+    }
+}
